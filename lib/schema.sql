a585fc3252c47ef5d1517f56cbebc87979f486ad
Eric Chrobak <eric@ericchrobak.com>
2018-08-06 21:03:25 +0000

    automatically backed up by learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -30,3 +30,9 @@ CREATE TABLE characters (
     series_id INTEGER,
     author_id INTEGER
 );
+
+CREATE TABLE character_books (
+    id PRIMARY KEY INTEGER,
+    book_id INTEGER,
+    character_id INTEGER
+);
