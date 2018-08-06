5c8452528a13f3a14efc90ec27e090137588d26c
Eric Chrobak <eric@ericchrobak.com>
2018-08-06 22:19:44 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -12,4 +12,4 @@ INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES (
 
 INSERT INTO character_books (id, book_id, character_id) VALUES (1, 1, 1), (2, 1, 2), (3, 2, 2), (4, 3, 2), (5, 1, 3), (6, 2, 3), (7, 3, 3), (8, 1, 4);
 
-INSERT INTO character_books (id, book_id, character_id) VALUES (9, 4, 5), (10, 4, 6), (11, 4, 6), (12, 4, 7), (13, 5, 5), (14, 5, 6), (15, 5, 7), (16, 5, 8), (13, 6, 5), (14, 6, 6), (15, 6, 7), (16, 6, 8);
+INSERT INTO character_books (id, book_id, character_id) VALUES (9, 4, 5), (10, 4, 6), (11, 4, 6), (12, 4, 7), (13, 5, 5), (14, 5, 6), (15, 5, 7), (16, 5, 8);
